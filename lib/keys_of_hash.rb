b0fd2086a3336206d5fc78cb5dcc4e8e25c266ab
Tracie Masek <traciemasek@gmail.com>
2019-07-28 14:52:38 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     keys_arr = []
     arguments.each do |arg|
-      if Hash.keys.include?(arg)
+      if Hash.new.keys.include?(arg)
         keys_arr << arg
       end
     end
